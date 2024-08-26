e229b439e0a476629ff1b97a322f12c293c31aff
maverikspectrum <smartec414+1@gmail.com>
2024-08-26 09:33:33 +0530

    Real Time chat app

--- src/lib/pusher.ts
+++ src/lib/pusher.ts
@@ -26,9 +26,9 @@ export const pusherServer =
 		appId: process.env.PUSHER_APP_ID!,
 		key: process.env.PUSHER_APP_KEY!,
 		secret: process.env.PUSHER_APP_SECRET!,
-		cluster: "eu",
+		cluster: "ap2",
 		useTLS: true,
 	});
 
 export const pusherClient =
-	global.pusherClient || new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, { cluster: "eu" });
+	global.pusherClient || new PusherClient(process.env.NEXT_PUBLIC_PUSHER_APP_KEY!, { cluster: "ap2" });
